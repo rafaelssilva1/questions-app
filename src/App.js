a788a3e7fb523747c37623158861965af5a78712
rafaelssilva1 <94930511+rafaelssilva1@users.noreply.github.com>
2022-07-10 16:50:10 +0100

    Share screen (#2)
    * setup-routes
    
    * setup-routes
    
    * routes-setup
    
    * loading-screen-logic
    
    * questions-list-screen
    
    * detail-screen
    
    * share screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 // components
-import LoadingScreen from './components/loading-screen';
+import LoadingScreen from "./components/loading-screen";
+import QuestionsListScreen from "./components/questions-list-screen";
+import DetailScreen from "./components/detail-screen";
 
 function App() {
+
   return (
     <>
       <Routes>
-        <Route index path="/" element={<LoadingScreen />}></Route>
+        <Route index path="/" element={<LoadingScreen />} />
+        <Route path="/questions" element={<QuestionsListScreen />} />
+        <Route path="/questions/:id" element={<DetailScreen />} />
       </Routes>
     </>
   );
---
+++ src/components/questions-list-screen.js
@@ -0,0 +1,74 @@
+import { useState , useEffect } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+
+
+import "./questions-list-screen.css";
+import DetailScreen from "./detail-screen";
+
+let offset = 0;
+
+export default function QuestionsListScreen() {
+    let [searchParams] = useSearchParams()
+    const term = searchParams.get("filter");
+
+    const [questions, setQuestions] = useState([]);
+
+
+    useEffect(() => {
+        const filterForm = document.querySelector("#filter");
+
+        fetch(`https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/questions?limit=10&offset=0?filter=${filterForm}`)
+            .then(response => response.json())
+            .then(data => setQuestions(data))
+
+        if(!(term)) {
+            filterForm.focus();
+        }
+        if(term) {
+            filterForm.value = term;
+        }
+    },[term]);
+
+    const loadMore = (e) => { //update currentPage var, make new API call and push to current array
+        const filterForm = document.querySelector("#filter");
+
+        offset = offset + 10
+
+        fetch(`https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/questions?limit=10&offset=${offset}?filter=${filterForm}`)
+            .then(response => response.json())
+            .then(data => {
+                setQuestions(questions.concat(data));
+            })
+    };
+
+    return (
+        <div className="container">
+            <div className="questions__title">
+                <h1>Questions List</h1>
+            </div>
+            <div>
+                <form method="GET">
+                    <input type="text" name="filter" id="filter" placeholder="Find a question here"></input>
+                </form>
+            </div>
+            <ul className="questions__list">
+                {questions.map(el => (
+                    <Link element={<DetailScreen />} to={`/questions/${el.id}`} key={el.id}>
+                        <li className="questions__item">
+                        <div className="questions__img">
+                            <img src={el.thumb_url} alt={el.question}></img>
+                        </div>
+                        <article className="questions__content">
+                            <h2>
+                                <span className="questions__text">{el.question}</span>
+                            </h2>
+                        </article>
+                        </li>
+                    </Link>
+                ))}
+            </ul>
+            <button onClick={loadMore}>Load More</button>
+        </div>
+    )
+
+}
