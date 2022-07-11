f3f3c1aeaeb29b5d7b9d6b31d06ba3059a3cbc00
unknown <rafaelssilva1994@gmail.com>
2022-07-11 17:24:37 +0100

    connectivity screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 // components
 import LoadingScreen from "./components/loading-screen";
 import QuestionsListScreen from "./components/questions-list-screen";
 import DetailScreen from "./components/detail-screen";
+import ConnectivityScreen from "./components/connectivity-screen";
 
 function App() {
+  const [connectivity, setConnectivity] = useState(false);
+
+  if(!navigator.onLine) {
+    setConnectivity(true);
+  }
 
   return (
     <>
+      {connectivity ? <ConnectivityScreen /> : ""}
       <Routes>
         <Route index path="/" element={<LoadingScreen />} />
         <Route path="/questions" element={<QuestionsListScreen />} />
