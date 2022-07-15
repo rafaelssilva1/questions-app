d74b390b3fe1e904be0fb2ce7b72bfc11e22dbf3
Rafael Silva <rafaelssilva1994@gmail.com>
2022-07-15 09:00:51 +0100

    minor fixes

--- src/components/share-screen.js
+++ src/components/share-screen.js
@@ -1,6 +1,8 @@
 import "./share-screen.css";
+import { useState } from "react";
  
 export default function ShareScreen({closeShare, styles}) {
+    const [shared, setShared] = useState(false);
 
     const closeShareBtn = () => {
         closeShare(false);
@@ -24,17 +26,27 @@ export default function ShareScreen({closeShare, styles}) {
                 .then(data => data);
                 
             document.querySelector(".share__form").reset();
-            closeShare(false);
+            setShared(true);
+
+            setTimeout(() => {
+                closeShare(false);
+                setShared(false);
+            }, 1500);
         }
     };
 
     return (
         <div className="share" id={styles ? "inverted" : ""}>
-            <h2 className="default__title">Share this question!</h2>
+            <h2 className="default__title">{styles ? "Share this search!" : "Share this question!"}</h2>
             <form className="share__form" onSubmit={shareQuestion}>
                 <input type="email" name="share__email" id="share__email" placeholder="Destination email..."></input>
                 <input type="submit" value="Share!" id={styles ? "invertedBtn" : "share__submit"}></input>
             </form>
+            {shared &&
+                <div>
+                    <p className="share__success">Shared successfully!</p>
+                </div>
+            }
             <div>
                 <span className="question__close material-symbols-outlined" onClick={closeShareBtn}>close</span>
             </div>
